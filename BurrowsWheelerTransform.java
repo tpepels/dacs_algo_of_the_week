ac71aa9e31e902eaf5dc0062d4a33426c9f174bf
Tom <tpepels@gmail.com>
2024-01-18 12:27:37 +0100

    Last algorithm

--- BurrowsWheelerTransform.java
+++ BurrowsWheelerTransform.java
@@ -1,8 +1,9 @@
 import java.util.Arrays;
 
+// Class to represent a rotation of the input string
 class Rotation implements Comparable<Rotation> {
-    int index;
-    String suffix;
+    int index; // The original index of the rotation
+    String suffix; // The suffix string for this rotation
 
     public Rotation(int index, String suffix) {
         this.index = index;
@@ -10,43 +11,45 @@ class Rotation implements Comparable<Rotation> {
     }
 
     @Override
-    public int compareTo(Rotation o) {
-        return this.suffix.compareTo(o.suffix);
+    public int compareTo(Rotation other) {
+        // Compare rotations based on their suffixes
+        return this.suffix.compareTo(other.suffix);
     }
 }
 
 public class BurrowsWheelerTransform {
 
+    // Compute the suffix array of the input text
     public static int[] computeSuffixArray(String inputText) {
         int lenText = inputText.length();
+        Rotation[] rotations = new Rotation[lenText];
 
-        Rotation[] suff = new Rotation[lenText];
-
+        // Create rotations and store them
         for (int i = 0; i < lenText; i++) {
-            suff[i] = new Rotation(i, inputText.substring(i));
+            rotations[i] = new Rotation(i, inputText.substring(i));
         }
 
-        Arrays.sort(suff);
+        // Sort the rotations alphabetically based on their suffixes
+        Arrays.sort(rotations);
 
+        // Extract and return the indices of the sorted rotations
         int[] suffixArr = new int[lenText];
         for (int i = 0; i < lenText; i++) {
-            suffixArr[i] = suff[i].index;
+            suffixArr[i] = rotations[i].index;
         }
 
         return suffixArr;
     }
 
-    public static String findLastChar(String inputText,
-            int[] suffixArr) {
-        int n = inputText.length();
-
+    // Find the last character of each rotation and construct the BWT array
+    public static String constructBWT(String inputText, int[] suffixArr) {
         StringBuilder bwtArr = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            int j = suffixArr[i] - 1;
-            if (j < 0) {
-                j = j + n;
+        for (int suffixIndex : suffixArr) {
+            int charIndex = suffixIndex - 1;
+            if (charIndex < 0) {
+                charIndex += inputText.length();
             }
-            bwtArr.append(inputText.charAt(j));
+            bwtArr.append(inputText.charAt(charIndex));
         }
 
         return bwtArr.toString();
@@ -55,12 +58,13 @@ public class BurrowsWheelerTransform {
     public static void main(String[] args) {
         String inputText = "banana$";
 
+        // Compute the suffix array for the input text
         int[] suffixArr = computeSuffixArray(inputText);
 
-        String bwtArr = findLastChar(inputText, suffixArr);
+        // Construct the Burrows-Wheeler Transform array using the suffix array
+        String bwtArr = constructBWT(inputText, suffixArr);
 
-        System.out.println("Input text : " + inputText);
-        System.out.println("Burrows - Wheeler Transform : "
-                + bwtArr);
+        System.out.println("Input text: " + inputText);
+        System.out.println("Burrows-Wheeler Transform: " + bwtArr);
     }
 }
